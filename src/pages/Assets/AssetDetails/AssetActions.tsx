1b4521febed33405ec38b9294e03c5abfebf95f0
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-09-01 15:22:55 -0600

    feat: refactor ModalProvider and wire up receive modal

--- src/pages/Assets/AssetDetails/AssetActions.tsx
+++ src/pages/Assets/AssetDetails/AssetActions.tsx
@@ -4,21 +4,19 @@ import { AssetMarketData } from '@shapeshiftoss/market-service'
 import { useModal } from 'context/ModalProvider/ModalProvider'
 
 export const AssetActions = ({ asset }: { asset: AssetMarketData }) => {
-  const modal = useModal()
+  const { send, receive } = useModal()
+  const { open: openSend } = send
+  const { open: openReceive } = receive
   return (
     <ButtonGroup
       ml={{ base: 0, lg: 'auto' }}
       mt={{ base: 6, lg: 0 }}
       width={{ base: 'full', lg: 'auto' }}
     >
-      <Button onClick={() => modal.open('send')} isFullWidth leftIcon={<ArrowUpIcon />}>
+      <Button onClick={() => openSend()} isFullWidth leftIcon={<ArrowUpIcon />}>
         Send
       </Button>
-      <Button
-        onClick={() => modal.open('receive', { asset })}
-        isFullWidth
-        leftIcon={<ArrowDownIcon />}
-      >
+      <Button onClick={() => openReceive({ asset })} isFullWidth leftIcon={<ArrowDownIcon />}>
         Receive
       </Button>
     </ButtonGroup>
